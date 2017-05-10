2d6944d9496bd9af78853e40651982c86b04b9c1
Josh Harris <joshua.harris1085@gmail.com>
2017-05-09 23:20:34 -0500

    Don't use boilerplate

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new Component. This Component should produce some HTML.
+// This is a class. Pass the instance to the dom render
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this Component's generated HTML and put it on the page (DOM)
+// <App /> is an instance of App. We are attaching it to the DOM in
+// the div by the name of "container"
+ReactDOM.render(<App />, document.querySelector('.container'));
