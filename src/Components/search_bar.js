974eb5d37cbb0866e2dfbda90185a6ad3baa6b82
Josh Harris <joshua.harris1085@gmail.com>
2017-05-09 23:56:37 -0500

    add searchbar

---
+++ src/Components/search_bar.js
@@ -0,0 +1,9 @@
+// Components need to import react because when we translate
+// jsx to js it turns into a call like react.CreateElement
+import React from 'react';
+
+const SearchBar = () => {
+  return <input />;
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './Components/search_bar';
+
+// Insert your youtube api key here
+const API_KEY = '';
 
 // Create a new Component. This Component should produce some HTML.
 // This is a class. Pass the instance to the dom render
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 // Take this Component's generated HTML and put it on the page (DOM)
