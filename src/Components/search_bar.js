17b3729e64c342d3b32f52f3bebe79556a48f15e
Josh Harris <joshua.harris1085@gmail.com>
2017-05-10 00:39:24 -0500

    Setup SearchBar state

--- src/Components/search_bar.js
+++ src/Components/search_bar.js
@@ -1,9 +1,34 @@
 // Components need to import react because when we translate
 // jsx to js it turns into a call like react.CreateElement
-import React from 'react';
 
-const SearchBar = () => {
-  return <input />;
+// Import React and pull of property Component as its own
+import React, { Component } from 'react';
+
+// class component and gives it react.component functionality
+class SearchBar extends Component {
+  // initialize state. Constructor called first on obj creationg for setup
+  //
+  constructor(props) {
+    // call parent method from component
+    super(props);
+    // initialize state. this.state will contain properties we want to hold
+    this.state = {term: ''};
+  }
+
+  // Must have defined render method for class components
+  render() {
+    //onChange is the input handler
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({term: event.target.value})}
+        />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
 }
 
 export default SearchBar;
